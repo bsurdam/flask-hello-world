0a271bba64b869aa5b1a8f2db2200372a6dda5cb
Brandon Surdam <brandon.surdam@colorado.edu>
2023-11-04 16:54:47 +0000

    Add / to URL

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('db_test')
+@app.route('/db_test')
 def db_test():
     conn = psycopg2.connect("postgres://brandons_database_user:fLu3o48CwPeX33S01QMCtyCs6kgie8RG@dpg-cl37412uuipc738889dg-a/brandons_database")
     conn.close()
