0e7ded2abb51f0cada701721826c49b09f9a4208
Brandon Surdam <brandon.surdam@colorado.edu>
2023-11-04 16:48:37 +0000

    DB TEST

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('db_test')
+def db_test():
+    conn = psycopg2.connect("postgres://brandons_database_user:fLu3o48CwPeX33S01QMCtyCs6kgie8RG@dpg-cl37412uuipc738889dg-a/brandons_database")
+    conn.close()
+    return "Database Connection Successful"
