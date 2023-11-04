d8d0c3efa60cf13e9d5ae5965ee8afaab423cf02
Brandon Surdam <brandon.surdam@colorado.edu>
2023-11-04 17:24:36 +0000

    Basketball Table Changes

--- app.py
+++ app.py
@@ -30,3 +30,18 @@ def db_create():
     conn.close()
     return "Basketball Table Creation Successful"
 
+@app.route('/db_insert')
+def db_insert():
+    conn = psycopg2.connect("postgres://brandons_database_user:fLu3o48CwPeX33S01QMCtyCs6kgie8RG@dpg-cl37412uuipc738889dg-a/brandons_database")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
