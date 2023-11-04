4b5488d7c31ba39c73927ed90ef5d5127cdc1f15
Brandon Surdam <brandon.surdam@colorado.edu>
2023-11-04 17:02:32 +0000

    Basketball Table

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def db_test():
     conn = psycopg2.connect("postgres://brandons_database_user:fLu3o48CwPeX33S01QMCtyCs6kgie8RG@dpg-cl37412uuipc738889dg-a/brandons_database")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def db_create():
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Creation Successful"
+
